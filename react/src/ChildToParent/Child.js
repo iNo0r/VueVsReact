2d42ed363c71186fee92023cd538a361c7e054f4
iNo0r <pic.ner101@gmail.com>
2022-08-16 03:35:46 +0100

    Update Child.js

--- react/src/ChildToParent/Child.js
+++ react/src/ChildToParent/Child.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+// function Child(props) {
 function Child({ handleButtonClicked }) {
+
   return (
     <div>
+//       <button onClick={() => props.handleButtonClicked()}>Submit</button>
       <button onClick={() => handleButtonClicked()}>Submit</button>
     </div>
   )
